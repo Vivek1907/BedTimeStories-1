d0bff08acb19f890a34ca79a387ee0cd269d98bb
Vivek1907 <57996278+Vivek1907@users.noreply.github.com>
2020-08-05 14:32:05 +0530

    Connected to firestore

--- screens/writeScreen.js
+++ screens/writeScreen.js
@@ -1,18 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, Alert} from 'react-native';
+import db from '../config';
 
 export default class WriteScreen extends React.Component {
     constructor() {
         super();
         this.state = {
-            storyValue: ''
+            storyValue: '',
+            storyTitle: '',
+            storyAuthor:''
         }
+    }
+
+    async submitStory(){
+        const storyTitle = this.state.storyTitle
+        const storyAuthor = this.state.storyAuthor
+        const storyValue = this.state.storyValue
+        await db.collection("stories").add({
+            'author': storyAuthor,
+            'story': storyValue,
+            'title':storyTitle
+        })
+        Alert.alert("Story Submitted");
     }
 
     render() {
         return (
-            <ScrollView contentContainerStyle={styles.container}>
+            <View style={styles.container}>
+            <ScrollView scrollEnabled>
+                <TextInput 
+                style={styles.singleLine}
+                onChangeText={(text)=>{
+                    this.setState({
+                        storyTitle: text
+                    })
+                }}
+                value={this.state.storyTitle}
+                multiline={false}
+                placeholder="Story Title"
+                />
+                <TextInput 
+                style={[styles.singleLine,{marginTop:20}]}
+                onChangeText={(text)=>{
+                    this.setState({
+                        storyAuthor: text
+                    })
+                }}
+                value={this.state.storyAuthor}
+                multiline={false}
+                placeholder="Story Author"
+                />
                 <TextInput
                     style={styles.input}
                     onChangeText={(text) => {
@@ -25,16 +62,14 @@ export default class WriteScreen extends React.Component {
                     value={this.state.storyValue}
                     placeholder="Write Your Story Here" />
                 <TouchableOpacity style={styles.button} onPress={()=>{
-                    this.setState({
-                        storyValue:''
-                    })
+                    this.submitStory();
                 }}>
                     <Text style={styles.text}>
                         Submit
                     </Text>
                 </TouchableOpacity>
             </ScrollView>
-
+            </View>
         )
     }
 }
@@ -49,7 +84,7 @@ const styles = StyleSheet.create({
     input: {
         marginTop: 30,
         width: 350,
-        height: 550,
+        height: 500,
         borderColor: 'gray',
         borderWidth: 1,
         justifyContent: 'flex-start',
@@ -64,11 +99,25 @@ const styles = StyleSheet.create({
         paddingVertical:20,
         backgroundColor:'#3498db',
         width:150,
-        borderRadius:20
+        borderRadius:20,
+        textAlign:'center',
+        alignSelf:'center'
     },
     text:{
         fontSize:20,
         color:'white',
-        fontWeight:'bold'
+        fontWeight:'bold',
+        
+    },
+    singleLine:{
+        borderWidth:1,
+        borderColor: 'gray',
+        borderWidth: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        textAlignVertical: 'top',
+        fontSize: 20,
+        width: 350,
+        marginTop:40
     }
 }) 
